ce0257641501c61e9b022fec42d72afc070c2925
Jordan Vasquez <jordanlakers223@gmail.com>
2020-03-28 20:49:33 -0700

    Made styling changes in JS

---
+++ app.js
@@ -0,0 +1,25 @@
+let oList = document.getElementById("o-List");
+  
+// This handler will be executed only once when the cursor
+// moves over the ordered list
+oList.addEventListener("mouseenter", function( event ) {   
+  // highlight the mouseenter target
+  event.target.style.color = "purple";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+oList.addEventListener("mouseover", function( event ) {   
+  // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
